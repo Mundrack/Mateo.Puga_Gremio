8ecad19b376e0fd04b0283c26f6ef063f2915ee9
Mundrack <mateopuga75@gmail.com>
2024-10-28 09:52:44 -0500

    Agregamos limitaciones y algunos requered a la clase de user que tambien la renombramos por libro

---
+++ Mateo.Puga-Examen Progreso I/Models/MP.Libro.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mateo.Puga_Examen_Progreso_I.Models
+{
+    public class MP
+    {
+        public int MP_LibroID { get; set; }
+
+        [requered]
+        [MaxLength(100)]
+        public string? MP_NombreLibro { get; set; }
+
+        [requered][MaxLength(100)]
+        public string? MP_Autor { get; set; }
+        [requered]
+
+        public decimal MP_HoraAgregacion { get; set; }
+        [Range(1, 15)]
+        public int MP_FechaPublicacion { get; set; }
+        [requered]
+        public bool MP_Nuevo {  get; set; }
+        
+    }
+}
---
+++ Mateo.Puga-Examen Progreso I/Models/requeredAttribute.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Mateo.Puga_Examen_Progreso_I.Models
+{
+    internal class requeredAttribute : Attribute
+    {
+    }
+}
