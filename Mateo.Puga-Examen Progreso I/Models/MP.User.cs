28a3b6f761e93fec7a48a47ea871c26ab7f69415
Mundrack <mateopuga75@gmail.com>
2024-10-28 09:33:56 -0500

    Creamos la clase usuario con algunos atributos

---
+++ Mateo.Puga-Examen Progreso I/Models/MP.User.cs
@@ -0,0 +1,12 @@
+﻿namespace Mateo.Puga_Examen_Progreso_I.Models
+{
+    public class MP
+    {
+        public int MP_LibroID { get; set; }
+        public string MP_Name { get; set; }
+        public string MP_Apellido { get; set; }
+        public decimal MP_estatura { get; set; }
+        
+
+    }
+}
