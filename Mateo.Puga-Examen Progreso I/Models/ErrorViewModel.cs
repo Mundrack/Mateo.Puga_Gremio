397d0654e6d3605bcfcd1d1e83d3fbc98f85170f
Mundrack <mateopuga75@gmail.com>
2024-10-28 09:30:10 -0500

    Add project files.

---
+++ Mateo.Puga-Examen Progreso I/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mateo.Puga_Examen_Progreso_I.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
